8c85767757e768bd43e441df2e697c8fca9d900a
BuildTools <unconfigured@null.spigotmc.org>
2020-07-13 11:01:13 +0100

    Added event handlers for clicking and item dragging

--- CodexFactions/src/org/codex/auctionhouse/AuctionCommand.java
+++ CodexFactions/src/org/codex/auctionhouse/AuctionCommand.java
@@ -11,11 +11,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryInteractEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
-
-import net.md_5.bungee.api.ChatColor;
 
 public class AuctionCommand implements CommandExecutor, Listener {
 	
@@ -28,15 +25,21 @@ public class AuctionCommand implements CommandExecutor, Listener {
 		if (sender instanceof Player && (sender.isOp() || sender.hasPermission("ah.use"))) {
 			Player player = (Player) sender;
 			
-			sender.sendMessage(ChatColor.GREEN + "Auction House Opened...");
-
-			
 			if (args.length == 0) {
+				
 				Inventory i = invs.containsKey(1) ? invs.get(1) : Bukkit.createInventory(null, 54, "Auction House");
 				player.openInventory(i);
 				invs.put(1, i); //saves player modifications
+	
 			}
-
+			
+			if (args.length > 1) {
+				
+				sender.sendMessage("Unknown arguments, use /ah [sell]");
+				
+			}else
+				
+			sender.sendMessage("You have executed /ah sell! KINGMO ADD ECO!");
 			
 		}else
 			sender.sendMessage("You cannot use this command in console!");
@@ -46,6 +49,10 @@ public class AuctionCommand implements CommandExecutor, Listener {
 	}
 	
 	
+	
+	
+	//Events below for AH
+	
 	@EventHandler
 	public void onPlayerClick (InventoryClickEvent e) {
 		
@@ -53,8 +60,17 @@ public class AuctionCommand implements CommandExecutor, Listener {
 			e.setCancelled(true);
 			
 		}
+			
+	}
+	
+	@EventHandler
+	public void onInventoryDrag (InventoryDragEvent e) {	
 		
-		
+		if (e.getInventory().getTitle().equals("Auction House")) {
+			e.setCancelled(true);
+
+		}
+
+		}
 		
 	}
-}
