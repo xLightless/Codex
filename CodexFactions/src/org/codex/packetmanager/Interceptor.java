c11f73ab513f5f605214aaab385c779bf2d0b1ad
BuildTools <firingvortex@gmail.com>
2020-07-22 22:44:24 +0100

    adding interceptor to begining of netty pipeline

--- CodexFactions/src/org/codex/packetmanager/Interceptor.java
+++ CodexFactions/src/org/codex/packetmanager/Interceptor.java
@@ -1,208 +1,28 @@
 package org.codex.packetmanager;
 
-import java.net.SocketAddress;
-
-import javax.crypto.SecretKey;
-
-import org.apache.logging.log4j.Marker;
-import org.apache.logging.log4j.MarkerManager;
-import org.bukkit.Bukkit;
 import org.bukkit.Server;
 
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
-import net.minecraft.server.v1_8_R3.EnumProtocol;
-import net.minecraft.server.v1_8_R3.EnumProtocolDirection;
-import net.minecraft.server.v1_8_R3.IChatBaseComponent;
-import net.minecraft.server.v1_8_R3.NetworkManager;
+import io.netty.channel.SimpleChannelInboundHandler;
 import net.minecraft.server.v1_8_R3.Packet;
-import net.minecraft.server.v1_8_R3.PacketListener;
 
-public class Interceptor extends NetworkManager {
 
-	public Server server;
-	public NetworkManager parent;
-
-	 public static final Marker a = MarkerManager.getMarker("NETWORK");
-	 public static final Marker b = MarkerManager.getMarker("NETWORK_PACKETS", NetworkManager.a);
-	 public Channel channel;
+public class Interceptor extends SimpleChannelInboundHandler<Packet<?>>{
 	
-	public Interceptor(EnumProtocolDirection enumprotocoldirection, Server serv, NetworkManager parent) {
-		super(enumprotocoldirection);
-		server = serv;
-		this.parent = parent;
-		channel = parent.channel;
-			System.err.println(channel.config().isAutoRead());
-		
-		System.err.println("HERE - " + channel);
+	Server server ;
+	
+	public Interceptor(Server server) {
+		this.server = server;
 	}
 
 	@Override
-	public void handle(@SuppressWarnings("rawtypes") Packet packet) {
-		PacketEvent e = new PacketEvent(packet);
-		if (server == null) {
-			System.err.println("Server is null ");
-			parent.handle(packet);
-			return;
-		}
-		server.getPluginManager().callEvent(e);
-		if (!e.isCancelled()) {
-			parent.handle(packet);
+	protected void channelRead0(ChannelHandlerContext arg0, Packet<?> arg1) throws Exception {
+		PacketEvent event = new PacketEvent(arg1);
+		server.getPluginManager().callEvent(event);
+		if(!event.isCancelled()) {
+			arg0.fireChannelRead(arg1);
 		}
 		
-
 	}
-
-	@Override
-	public void handlerAdded(ChannelHandlerContext arg0) throws Exception {
-		parent.handlerAdded(arg0);
-	}
-
-	@Override
-	public void handlerRemoved(ChannelHandlerContext arg0) throws Exception {
-		parent.handlerRemoved(arg0);
-	}
-
-	@Override
-	public void a() {
-		parent.a();
-	}
-
-
-	@Override
-	public void a(EnumProtocol enumprotocol) {
-		parent.a(enumprotocol);
-		this.channel = parent.channel;
-	}
-
-	@Override
-	public void a(int i) {
-		parent.a(i);
-	}
-
-	@Override
-	public void a(Packet packet, GenericFutureListener<? extends Future<? super Void>> genericfuturelistener,
-			GenericFutureListener<? extends Future<? super Void>>... agenericfuturelistener) {
-		parent.a(packet, genericfuturelistener, agenericfuturelistener);
-	}
-	@Override
-	public void a(PacketListener packetlistener) {
-		parent.a(packetlistener);
-	}
-
-	@Override
-	public void a(SecretKey secretkey) {
-		parent.a(secretkey);
-	}
-
-	@Override
-	public boolean acceptInboundMessage(Object arg0) throws Exception {
-		return parent.acceptInboundMessage(arg0);
-	}
-
-	@Override
-	public boolean c() {
-		return parent.c();
-	}
-
-	@Override
-	public void channelActive(ChannelHandlerContext channelhandlercontext) throws Exception {
-		parent.channelActive(channelhandlercontext);
-		this.channel = parent.channel;
-	}
-
-	@Override
-	public void channelInactive(ChannelHandlerContext channelhandlercontext) throws Exception {
-		parent.channelInactive(channelhandlercontext);
-	}
-
-	@Override
-	public void channelRead(ChannelHandlerContext arg0, Object arg1) throws Exception {
-		parent.channelRead(arg0, arg1);
-	}
-
-
-	@Override
-	public void channelReadComplete(ChannelHandlerContext arg0) throws Exception {
-		parent.channelReadComplete(arg0);
-	}
-
-	@Override
-	public void channelRegistered(ChannelHandlerContext arg0) throws Exception {
-		parent.channelRegistered(arg0);
-	}
-
-	@Override
-	public void channelUnregistered(ChannelHandlerContext arg0) throws Exception {
-		parent.channelUnregistered(arg0);
-	}
-
-	@Override
-	public void channelWritabilityChanged(ChannelHandlerContext arg0) throws Exception {
-		parent.channelWritabilityChanged(arg0);
-	}
-
-	@Override
-	public void close(IChatBaseComponent ichatbasecomponent) {
-		parent.close(ichatbasecomponent);
-	}
-
-	@Override
-	public void exceptionCaught(ChannelHandlerContext channelhandlercontext, Throwable throwable) throws Exception {
-		parent.exceptionCaught(channelhandlercontext, throwable);
-	}
-
-	@Override
-	public boolean g() {
-		return parent.g();
-	}
-
-	@Override
-	public PacketListener getPacketListener() {
-		return parent.getPacketListener();
-	}
-
-	@Override
-	public SocketAddress getRawAddress() {
-		return parent.getRawAddress();
-	}
-
-	@Override
-	public SocketAddress getSocketAddress() {
-		return parent.getSocketAddress();
-	}
-
-	@Override
-	public boolean h() {
-		return parent.h();
-	}
-
-	@Override
-	public boolean isSharable() {
-		return parent.isSharable();
-	}
-
-	@Override
-	public IChatBaseComponent j() {
-		return parent.j();
-	}
-
-	@Override
-	public void k() {
-		parent.k();
-	}
-
-	@Override
-	public void l() {
-		parent.l();
-	}
-
-	@Override
-	public void userEventTriggered(ChannelHandlerContext arg0, Object arg1) throws Exception {
-		parent.userEventTriggered(arg0, arg1);
-	}
-	
 
 }
--- CodexFactions/src/org/codex/packetmanager/PacketMain.java
+++ CodexFactions/src/org/codex/packetmanager/PacketMain.java
@@ -2,7 +2,6 @@ package org.codex.packetmanager;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
@@ -11,10 +10,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
 
 import net.minecraft.server.v1_8_R3.EntityPlayer;
-import net.minecraft.server.v1_8_R3.EnumProtocolDirection;
 import net.minecraft.server.v1_8_R3.NetworkManager;
 import net.minecraft.server.v1_8_R3.Packet;
 import net.minecraft.server.v1_8_R3.PlayerConnection;
@@ -26,31 +23,19 @@ public class PacketMain implements Listener {
 	public PacketMain(Server server) {
 		this.server = server;
 	}
-	
+
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
-		/*Player p = e.getPlayer();
+		Player p = e.getPlayer();
 		if (p instanceof CraftPlayer) {
 			CraftPlayer cp = (CraftPlayer) p;
 			EntityPlayer ep = cp.getHandle();
-			try {
-				Field field = PlayerConnection.class.getDeclaredField("networkManager");
-				field.setAccessible(true);
-				Field modifiersField = Field.class.getDeclaredField("modifiers");
-				modifiersField.setAccessible(true);
-				modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-				field.set(ep.playerConnection, new Interceptor(EnumProtocolDirection.CLIENTBOUND, server, (NetworkManager) field.get(ep.playerConnection)));
-				System.out.println("Testing " + ep.joining);
-				ep.playerConnection.isDisconnected();
-			} catch (Throwable err) { 
-				err.printStackTrace();
-			}
+			NetworkManager nm = ep.playerConnection.networkManager;
+			nm.channel.pipeline().addFirst(new Interceptor(server));
 
 		}
-		*/
-		
-	} 
-	
+
+	}
 
 	public static void sendPlayerPacket(Player p, Packet<?> packet) {
 		if (p instanceof CraftPlayer) {
@@ -73,19 +58,24 @@ public class PacketMain implements Listener {
 	}
 
 	public static void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
-	        try {
-	            Object enumTitle = getNMSClass("PacketPlayOutTitle").getDeclaredClasses()[0].getField("TITLE").get(null);
-	            Object enumSubTitle = getNMSClass("PacketPlayOutTitle").getDeclaredClasses()[0].getField("SUBTITLE").get(null);
-	            Object chat = getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", String.class).invoke(null, "{\"text\":\""+title+"\"}");
-	            Object subchat = getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", String.class).invoke(null, "{\"text\":\""+subtitle+"\"}");
-	            Constructor<?> titleConstructor = getNMSClass("PacketPlayOutTitle").getConstructor(getNMSClass("PacketPlayOutTitle").getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"), int.class, int.class, int.class);
-	            Packet<?> packet = (Packet<?>) titleConstructor.newInstance(enumTitle, chat, fadeIn, stay, fadeOut);
-	            Packet<?> packet2 = (Packet<?>) titleConstructor.newInstance(enumSubTitle, subchat, fadeIn, stay, fadeOut);
-	            sendPlayerPacket(player, packet);
-	            sendPlayerPacket(player, packet2);
-	    }catch(Exception e) {
-	    	e.printStackTrace();
-	    }
-	        
+		try {
+			Object enumTitle = getNMSClass("PacketPlayOutTitle").getDeclaredClasses()[0].getField("TITLE").get(null);
+			Object enumSubTitle = getNMSClass("PacketPlayOutTitle").getDeclaredClasses()[0].getField("SUBTITLE")
+					.get(null);
+			Object chat = getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", String.class)
+					.invoke(null, "{\"text\":\"" + title + "\"}");
+			Object subchat = getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", String.class)
+					.invoke(null, "{\"text\":\"" + subtitle + "\"}");
+			Constructor<?> titleConstructor = getNMSClass("PacketPlayOutTitle").getConstructor(
+					getNMSClass("PacketPlayOutTitle").getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"),
+					int.class, int.class, int.class);
+			Packet<?> packet = (Packet<?>) titleConstructor.newInstance(enumTitle, chat, fadeIn, stay, fadeOut);
+			Packet<?> packet2 = (Packet<?>) titleConstructor.newInstance(enumSubTitle, subchat, fadeIn, stay, fadeOut);
+			sendPlayerPacket(player, packet);
+			sendPlayerPacket(player, packet2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
